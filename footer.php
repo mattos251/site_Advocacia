e8b501b8f0910e46d14b6c41f698f59e578d15ec
AckermanFernando <fernandoloiola03@gmail.com>
2021-02-03 18:15:24 -0300

    first commit

--- footer.php
+++ footer.php
@@ -1,4 +1,33 @@
 <?php wp_footer(); ?>
-footer
+<div class="footer">
+    <div class="FooterTitulo">
+        <h1>DADOS PARA CONTATO</h1>
+    </div>
+
+    <div class="FooterInformacoes">
+        <div class="Atendimento" class="Center">
+            <h2>ATENDIMENTO</h2>
+            <div class="Barra"></div>
+            <p>WhatsApp: (11) 989738230</p>
+            <p>E-mail: contato@webpress.net.br</p>
+            <p>Seg à Sex das 08:00 as 17:00</p>
+        </div>
+
+        <div class="RedeSocial" class="Center">
+            <h2>SIGA-NOS NAS REDES SOCIAIS</h2>
+            <div class="Barra"></div>
+        </div>
+
+        <div class="Endereco" class="Center">
+            <h2>ENDEREÇO</h2>
+            <div class="Barra"></div>
+            <p>Av. Augusto de Lima, 479, 6º andar,</p> 
+            <p>secretaria sala 610</p>
+            <p>CEP: 30190-005</p>
+            <p>Centro - Belo Horizonte | MG</p>
+        </div>
+    </div>
+    
+</div>
 </body>
 </html>
--- header.php
+++ header.php
@@ -17,19 +17,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="pingback" href="<?php bloginfo('pingback_url');?>">
     <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Charm:wght@400;700&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+    <div class="home">
+        <div class="TopFaixa">         
+            <div class="LogoMarca">
+                <?php the_custom_logo(); ?>
+            </div>
 
-    <div class="TopFaixa">
-        <div class="Logomarca">
-            <?php the_custom_logo(); ?>
+            <nav class="MenuSup">
+                <a href="#">HOME</a>
+                <a href="#">CONTATO</a>
+                <a href="#">SOBRE</a>
+            </nav>  
+
+            <div class="Telefone"></div>
+        </div>  
+        
+        <div class="MidFaixaVerde">
+            <div class="MidFaixaAmarelo"></div>
         </div>
+
+        <div class="AreaPrincipal">
+            <p> <h1>Francisco Chagas Oliveira</h1></p>
+            <p><h1>Advogado</h1></p>
+            <p><h1>OAB-SP:43.9472</h1></p>
+        </div>
+
     </div>
-    
-    <div class="MidFaixaVerde">
-        <div class="MidFaixaAmarelo"></div>
+
+    <div class="Artigos">
+
     </div>
-    
+
+    <div class="Palestras">
+
+    </div>
     
     
