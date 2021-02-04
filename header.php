a0b3b7b7f122f6cc067550aa34309999942cfc87
AckermanFernando <fernandoloiola03@gmail.com>
2021-02-04 00:41:16 -0300

    Adiconando o botão de menu para smartphones e corrigindo a responsovidade do footer

--- header.php
+++ header.php
@@ -27,24 +27,34 @@
             <div class="LogoMarca">
                 <?php the_custom_logo(); ?>
             </div>
-
+        
             <nav class="MenuSup">
                 <a href="#">HOME</a>
+                <div class="Barra"></div>
                 <a href="#">CONTATO</a>
                 <a href="#">SOBRE</a>
             </nav>  
 
             <div class="Telefone"></div>
+
+            <div class="BotaoMobile">
+                <img src="<?php echo $template_directory;?>http://localhost/wp-content/uploads/2021/02/BotaoMobile.png" alt="Menu para mobile" title="Menu mobile">
+            </div>
         </div>  
         
         <div class="MidFaixaVerde">
             <div class="MidFaixaAmarelo"></div>
         </div>
+        <div class="PaginaInicial">
+            <div class="LogoBalanca">
+                <img src="<?php echo $template_directory;?>http://localhost/wp-content/uploads/2021/02/justice-2747368_640-1.png" alt="Menu " title="Menu ">
+            </div> 
 
-        <div class="AreaPrincipal">
-            <p> <h1>Francisco Chagas Oliveira</h1></p>
-            <p><h1>Advogado</h1></p>
-            <p><h1>OAB-SP:43.9472</h1></p>
+            <div class="AreaPrincipal">
+                <p> <h1>Francisco Chagas Oliveira</h1></p>
+                <p><h1>Advogado</h1></p>
+                <p><h1>OAB-SP:43.9472</h1></p>
+            </div>
         </div>
 
     </div>
