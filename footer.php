e2f741c247349f2455648db39f289f8cf71cee40
AckermanFernando <fernandoloiola03@gmail.com>
2021-02-10 00:20:37 -0300

    adicionando redes sociais

--- footer.php
+++ footer.php
@@ -4,29 +4,49 @@
         <h1>DADOS PARA CONTATO</h1>
     </div>
 
-    <div class="FooterInformacoes">
+    <div id="FooterInformacoes">
         <div class="Atendimento" class="Center">
             <h2>ATENDIMENTO</h2>
             <div class="Barra"></div>
-            <p>WhatsApp: (11) 989738230</p>
-            <p>E-mail: contato@webpress.net.br</p>
+            <p>WhatsApp: (11) 98973-8230</p>
+            <p>E-mail:  franciscoolivei82@hotmail.com</p>
             <p>Seg à Sex das 08:00 as 17:00</p>
         </div>
 
         <div class="RedeSocial" class="Center">
             <h2>SIGA-NOS NAS REDES SOCIAIS</h2>
             <div class="Barra"></div>
+            <div class="RedeSocialImages">
+                <div class="RedeSocialCima">
+                    <a href="" target="_blank"><img src="<?php echo $template_directory;?>http://localhost/wp-content/uploads/2021/02/twitter-2898665.png" alt="link twitter"></a>
+                    <a href="https://www.facebook.com/francisco.oliveira.56027" target="_blank"><img src="<?php echo $template_directory;?>http://localhost/wp-content/uploads/2021/02/icon-facebook-2898664-1.png" alt="Link facebook"></a>
+                </div>
+                <div class="RedeSocialBaixo">
+                    <a href="" target="_blank"><img src="<?php echo $template_directory;?>http://localhost/wp-content/uploads/2021/02/instagram.png" alt="Link facebook"></a>
+                    <a href="https://www.linkedin.com/in/francisco-chagas-oliveira-9a487a63" target="_blank"><img src="<?php echo $template_directory;?>http://localhost/wp-content/uploads/2021/02/linkedin.png" alt="Link facebook"></a>
+                </div>
+            </div>
         </div>
 
         <div class="Endereco" class="Center">
             <h2>ENDEREÇO</h2>
             <div class="Barra"></div>
-            <p>Av. Augusto de Lima, 479, 6º andar,</p> 
-            <p>secretaria sala 610</p>
-            <p>CEP: 30190-005</p>
-            <p>Centro - Belo Horizonte | MG</p>
+            <div class="EnderecoSp">
+                    <p>Endereço em São Paulo:</p>
+
+                    <p>Av. Maria Ricci Perrota -</p>
+                    <p>Vila Alzira, Guarulhos - SP</p>
+                    <p>CEP: 07210-420</p>
+            </div>
+
+            <div class="EnderecoCp">
+                <p>Endereço no Piauí:</p>
+
+                <p>Rua João Celestino da Silva, S/N, </p>
+                <p>Timon, Campinas do Piauí, PI.</p> 
+                <p>CEP:64730-000</p>
+            </div>
         </div>
-    </div>
     
 </div>
 </body>
--- header.php
+++ header.php
@@ -14,28 +14,41 @@
     <meta name="Description" content="<?php bloginfo("description")?>">
     <meta name="keywords" content="advocacia, advogado, processos"> 
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="pingback" href="<?php bloginfo('pingback_url');?>">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Charm:wght@400;700&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
-    <div class="home">
+<body <?php body_class(); ?> class="TelaCheia">
+    <header>
         <div class="TopFaixa">         
             <div class="LogoMarca">
                 <?php the_custom_logo(); ?>
             </div>
         
             <nav class="MenuSup">
-                <a href="#">HOME</a>
-                <div class="Barra"></div>
-                <a href="#">CONTATO</a>
-                <a href="#">SOBRE</a>
-            </nav>  
 
-            <div class="Telefone"></div>
+                <div class="MenuHome">
+                    <a href="#PaginaInicial">HOME</a>
+                    <div class="Barra"></div>
+                </div>
+
+                <div class="MenuArtigos">
+                    <a href="#Artigos">ARTIGOS</a>
+                    <div class="Barra"></div>
+                </div>
+
+                <div class="MenuPalestras">
+                    <a href="#Palestras">PALESTRAS</a>
+                    <div class="Barra"></div>
+                </div>
+
+                <div class="Telefone">
+                    <a href="#FooterInformacoes"><img src="<?php echo $template_directory;?>http://localhost/wp-content/uploads/2021/02/social-networking-icon-2898676.png" alt="telefone logo"></a>
+                </div>
+            </nav>  
 
             <div class="BotaoMobile">
                 <img src="<?php echo $template_directory;?>http://localhost/wp-content/uploads/2021/02/BotaoMobile.png" alt="Menu para mobile" title="Menu mobile">
@@ -45,26 +58,45 @@
         <div class="MidFaixaVerde">
             <div class="MidFaixaAmarelo"></div>
         </div>
-        <div class="PaginaInicial">
-            <div class="LogoBalanca">
-                <img src="<?php echo $template_directory;?>http://localhost/wp-content/uploads/2021/02/justice-2747368_640-1.png" alt="Menu " title="Menu ">
-            </div> 
+    </header>
+    <main>
+        <!-- <div class="EspacoSup"></div> -->
+        <div id="PaginaInicial">
+                <div class="LogoBalanca">
+                    <img src="<?php echo $template_directory;?>http://localhost/wp-content/uploads/2021/02/justice-2747368_640-1.png" alt="Menu " title="Menu ">
+                </div> 
 
-            <div class="AreaPrincipal">
-                <p> <h1>Francisco Chagas Oliveira</h1></p>
-                <p><h1>Advogado</h1></p>
-                <p><h1>OAB-SP:43.9472</h1></p>
+                <div class="AreaPrincipal">
+                    <p><h1>Francisco Chagas Oliveira</h1></p>
+                    <p><h1>Advogado</h1></p>
+                    <p><h1>OAB-SP:43.9472</h1></p>
+                </div>
+            </div>  
+    </main>
+    <div id="Artigos">
+        
+    </div>
+
+    <div id="Palestras">
+        <div class="PalestrasTitulo">
+            <h1>PALESTRAS</h1>
+        </div>
+        <div class="PalestrasVideo">
+            <div class="VideoPrincipal">
+                <iframe width="720" height="450" src="https://www.youtube.com/embed/yw6i1SAHetc?start=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <h2>twenty one pilots - Cancer (My Chemical Romance Cover) [Official Lyric Video]</h2>
+            </div>
+            <div class="ListaVideos">
+                <iframe width="168" height="94" src="https://www.youtube.com/embed/Pmv8aQKO6k0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="168" height="94" src="https://www.youtube.com/embed/gOdBRGvXhkM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="168" height="94" src="https://www.youtube.com/embed/RSAoJJzVXYY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="168" height="94" src="https://www.youtube.com/embed/j_MlBCb9-m8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            
+            <div class="BotaoProximo">
+                <button>PRÓXIMO</button>
             </div>
         </div>
-
-    </div>
-
-    <div class="Artigos">
-
-    </div>
-
-    <div class="Palestras">
-
     </div>
     
     
